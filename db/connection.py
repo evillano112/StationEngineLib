5460d2c067a68243b2c89503bcc310ea7e24bf01
Edward Villano <evill5@unh.newhaven.edu>
2026-03-02 13:09:37 -0500

    changed db connection to env variables. Really the db creds shouldn't be public, but it's okay for now

--- db/connection.py
+++ db/connection.py
@@ -1,9 +1,14 @@
+import os
+from dotenv import load_dotenv
 import mysql.connector
+
+# Load environment variables from .env file
+load_dotenv()
 
 def getConnection():
     return mysql.connector.connect(
-        host="localhost",
-        user="srv_library_conn",
-        password="xt4l4ct1um",
-        database="radio_db"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
